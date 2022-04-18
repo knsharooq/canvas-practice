ee284961878df4079f505b9705042764fb67d17a
SHAROOQ K N <sharooq@lilacinfotech.com>
2022-04-18 10:06:11 +0530

    platforms

--- index.js
+++ index.js
@@ -35,10 +35,10 @@ class Player {
 }
 
 class Platform {
-    constructor() {
+    constructor({x, y}) {
         this.position = {
-            x: 200,
-            y: 300
+            x,
+            y
         }
         this.width = 200
         this.height = 20
@@ -50,7 +50,10 @@ class Platform {
 }
 
 const player = new Player()
-const platform = new Platform()
+const platforms = [
+    new Platform({x:200, y:300}), 
+    new Platform({x:500, y:300})
+]
 
 const keys = {
     right: {
@@ -66,22 +69,24 @@ function animate() {
     c.clearRect(0, 0, canvas.width, canvas.height)
 
     player.update()
-    platform.draw()
+    platforms.forEach(platform => platform.draw())
 
     if(keys.right.pressed && player.position.x < 450) player.velocity.x = 5
     else if(keys.left.pressed && player.position.x > 100) player.velocity.x = -5 
     else {
         player.velocity.x = 0
-        if(keys.right.pressed) platform.position.x -= 5
-        else if(keys.left.pressed) platform.position.x += 5
+        if(keys.right.pressed) platforms.forEach(platform => platform.position.x -= 5) 
+        else if(keys.left.pressed) platforms.forEach(platform => platform.position.x += 5)
     }
 
     // platform collision detection
+    platforms.forEach(platform => {
     if(player.position.y + player.height <= platform.position.y && 
        player.position.y + player.height + player.velocity.y >= platform.position.y &&
        player.position.x + player.width >= platform.position.x &&
        player.position.x <= platform.position.x + platform.width
     ) player.velocity.y = 0 ;
+    })
 }
 
 animate()
